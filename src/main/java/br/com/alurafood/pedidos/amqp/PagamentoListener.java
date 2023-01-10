d6dc5fab8b643a923c967cf6a5f4e82402fce961
vinicius.alkimin <viniciusg_alkimin@hotmail.com>
2023-01-10 20:07:06 -0300

    Implementando consumo da fila para alterar pagamento

--- src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
+++ src/main/java/br/com/alurafood/pedidos/amqp/PagamentoListener.java
@@ -1,13 +1,16 @@
 package br.com.alurafood.pedidos.amqp;
 
 import br.com.alurafood.pedidos.dto.PagamentoDto;
-import org.springframework.amqp.core.Message;
+import br.com.alurafood.pedidos.service.PedidoService;
+import lombok.AllArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class PagamentoListener {
+
+    private PedidoService pedidoService;
 
 /*
     @RabbitListener(queues = "pagamento.concluido")
@@ -17,8 +20,9 @@ public class PagamentoListener {
 */
     @RabbitListener(queues = "pagamento.concluido")
     public void recebeMensagem(PagamentoDto pagamento) {
-        String msg = "Dados do pagamento recebido: " +
+/*        String msg = "Dados do pagamento recebido: " +
                 "Nome: %s, Numero do pedido: %s, Valor: R$ %s".formatted(pagamento.getNome(), pagamento.getNumero(), pagamento.getValor());
-        System.out.println(msg);
+        System.out.println(msg);*/
+        pedidoService.aprovaPagamentoPedido(pagamento.getPedidoId());
     }
 }
